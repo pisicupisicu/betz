2739bb3c230b70667e9da728e67477f84b2248ef
Radu Ispas <pisicu.pisicu@gmail.com>
2016-05-05 21:35:13 +0000

    changed menu

--- app/modules/livescore/livescore.php
+++ app/modules/livescore/livescore.php
@@ -37,13 +37,15 @@ class Livescore extends Module
         $this->CI->admin_navigation->child_link('livescore', 10, 'List competitions', site_url('admincp/livescore/list_competitions'));
         $this->CI->admin_navigation->child_link('livescore', 20, 'List teams', site_url('admincp/livescore/list_teams'));
         $this->CI->admin_navigation->child_link('livescore', 29, 'List matches pre', site_url('admincp3/livescore/list_matches_pre'));
-        $this->CI->admin_navigation->child_link('livescore', 30, 'List matches', site_url('admincp/livescore/list_matches'));
-        $this->CI->admin_navigation->child_link('livescore', 31, 'List matches with goals and cards', site_url('admincp7/livescore/list_matches'));
+        $this->CI->admin_navigation->child_link('livescore', 30, 'List matches today', site_url('admincp3/livescore/list_matches_today'));
+        $this->CI->admin_navigation->child_link('livescore', 31, 'List matches', site_url('admincp/livescore/list_matches'));
+        $this->CI->admin_navigation->child_link('livescore', 32, 'List matches with goals and cards', site_url('admincp7/livescore/list_matches'));
         $this->CI->admin_navigation->child_link('livescore', 40, 'Bets List', site_url('admincp2/livescore/list_bets'));
         $this->CI->admin_navigation->child_link('livescore', 50, 'Admin Tools', site_url('admincp2/livescore/admin_tools'));
         $this->CI->admin_navigation->child_link('livescore', 60, 'Parse Matches', site_url('admincp3/livescore/parse_matches'));
-        $this->CI->admin_navigation->child_link('livescore', 70, 'Stats Bets', site_url('admincp4/livescore/profit_loss_stats'));
-        $this->CI->admin_navigation->child_link('livescore', 80, 'Stats Results', site_url('admincp6/livescore/list_statistics'));
+        $this->CI->admin_navigation->child_link('livescore', 70, 'Parse Matches Today', site_url('admincp3/livescore/parse_matches_today'));
+        $this->CI->admin_navigation->child_link('livescore', 80, 'Stats Bets', site_url('admincp4/livescore/profit_loss_stats'));
+        $this->CI->admin_navigation->child_link('livescore', 90, 'Stats Results', site_url('admincp6/livescore/list_statistics'));
     }
 
-}
+} 
